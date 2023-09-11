509666a3881e09e803cf28b6a455efa9bb42f677
Sush25N <sushgabhane123@gmail.com>
2023-09-12 01:34:44 +0530

    components and navbar

--- back-road-trips/src/App.js
+++ back-road-trips/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Services from './components/Services'
+import Tours from './components/Tours'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 
 
 const App = () => {
   return (
     <>
-      <h1>Backroad app</h1>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
     </>
   )
 }
