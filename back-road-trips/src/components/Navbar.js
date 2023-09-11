9000398003def4cffd468b57c255571105180d61
Sush25N <sushgabhane123@gmail.com>
2023-09-12 02:11:35 +0530

    serives comp

--- back-road-trips/src/components/Navbar.js
+++ back-road-trips/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from "../assets/logo.svg"
-import pageLinks from '../data'
+import { pageLinks, socialLinks } from '../data'
 
 const Navbar = () => {
   return (
@@ -25,21 +25,17 @@ const Navbar = () => {
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-facebook"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-twitter"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-squarespace"></i
-            ></a>
-          </li>
+          {socialLinks.map((link) => {
+            // destructuring method
+            const {id, href, icon} = link;
+            return(
+                <li key={id}>
+                    <a href={href} target='_blank' rel="noreferrer" className='nav-icon'>
+                        <i className={icon}></i>
+                    </a>
+                </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
--- back-road-trips/src/data.js
+++ back-road-trips/src/data.js
@@ -1,8 +1,13 @@
-const pageLinks = [
+export const pageLinks = [
     { id: 1, href: "#home", text: 'home' },
     { id: 2, href: "#about", text: 'about' },
     { id: 3, href: "#services", text: 'services' },
     { id: 4, href: "#tours", text: 'tours' },
 ];
 
-export default pageLinks ;
+
+export const socialLinks = [
+    { id: 1, href: 'https://twitter.com', icon: 'fab fa-facebook' },
+    { id: 2, href: 'https://twitter.com', icon: 'fab fa-twitter' },
+    { id: 3, href: 'https://twitter.com', icon: 'fab fa-squarespace' },
+];
